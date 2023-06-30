53b2e2ddca7b0799b0c62c4d849432e8321fdf88
hovhannes12 <hovhannes.d3@tumo.org>
2023-06-30 19:02:17 +0400

    first commit

---
+++ game/index.js
@@ -0,0 +1,102 @@
+let cells = document.querySelectorAll(".cell");
+let statusText = document.getElementById("statusText");
+let restartBtn = document.getElementById("restartBtn");
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+let options = ["", "", "", "", "", "", "", "", "",];
+let currentPlayer = "X";
+let running = false;
+
+initializeGame();
+
+function initializeGame() {
+  cells.forEach(cell => cell.addEventListener("click", cellClicked))
+  restartBtn.addEventListener(
+    "click", function () {
+      location.reload();
+  });
+  statusText.textContent = `${currentPlayer}'s turn`;
+  running = true;
+
+}
+function cellClicked() {
+  const cellIndex = this.getAttribute("cellIndex");
+
+  if (options[cellIndex] != "" || !running) {
+    return;
+  }
+
+  updateCell(this, cellIndex);
+  checkWinner();
+
+}
+function updateCell(cell, index) {
+  
+  options[index] = currentPlayer;
+  cell.textContent = currentPlayer;
+
+
+}
+function changePlayer() {
+  currentPlayer = (currentPlayer == "X") ? "O" : "X";
+  statusText.textContent = `${currentPlayer}'s turn`;
+
+
+}
+function checkWinner() {
+  let roundWon = false;
+
+  for (let i = 0; i < winConditions.length; i++) {
+    const condation = winConditions[i];
+    const cellA = options[condation[0]];
+    const cellB = options[condation[1]];
+    const cellC = options[condation[2]];
+
+    if (cellA == "" || cellB == "" || cellC == "") {
+      continue;
+    }
+    if (cellA == cellB && cellB == cellC) {
+      roundWon = true;
+      break;
+    }
+  }
+  if (roundWon) {
+    statusText.textContent = `${currentPlayer} wins!`;
+    running = false;
+  }
+  else if (!options.includes("")) {
+    statusText.textContent = `Draw!`;
+    running = false;
+  }
+  else {
+    changePlayer();
+  }
+}
+function restartGame() {
+  currentPlayer = "X";
+  option = ["", "", "", "", "", "", "", "", "",];
+  statusText.textContent = `${currentPlayer}'s turn`;
+  cells.forrEach(cell => cell.textContent = "");
+  running = true;
+
+
+}
+
+function timer(){
+  setTimeout(finish, seconds * 1000);
+  getElement("time").innerHTML = seconds;
+  let countdown = setInterval(function () {
+    seconds--;
+    getElement("time").textContent = seconds;
+    if (seconds <= 0) clearInterval(countdown);
+    if (seconds === 5) getElement("time").style.color = "#ff0000";
+  }, 1000)
+}
